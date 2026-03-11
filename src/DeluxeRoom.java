3b4a2bb79c7cfc4baebe9a07e9b084782c5ced07
azizqasim <azozjungle@gmail.com>
2026-03-11 22:23:56 +0300

    Update DeluxeRoom.java

--- src/DeluxeRoom.java
+++ src/DeluxeRoom.java
@@ -1,4 +1,41 @@
+public class DeluxeRoom extends StandardRoom {
 
-public class DeluxeRoom {
+    private boolean hasJacuzzi;
+    private boolean hasOceanView;
+    private double luxurySurcharge;
 
+    public DeluxeRoom(int roomNumber, double price,
+                      int beds, boolean tv,
+                      boolean jacuzzi, boolean ocean,
+                      double surcharge) {
+
+        super(roomNumber, price, beds, tv);
+        this.hasJacuzzi = jacuzzi;
+        this.hasOceanView = ocean;
+        this.luxurySurcharge = surcharge;
+    }
+
+    public boolean hasJacuzzi() {
+        return hasJacuzzi;
+    }
+
+    public boolean hasOceanView() {
+        return hasOceanView;
+    }
+
+    public double getLuxurySurcharge() {
+        return luxurySurcharge;
+    }
+
+    public double getEffectivePrice() {
+        return pricePerNight + luxurySurcharge;
+    }
+
+    public String toString() {
+
+        return super.toString()
+                + " | Jacuzzi: " + hasJacuzzi
+                + " | Ocean View: " + hasOceanView
+                + " | Extra: " + luxurySurcharge;
+    }
 }
