848e2e4931f6b5b9f4c6fb1b2515fcf3365b56df
azizqasim <azozjungle@gmail.com>
2026-03-11 22:21:46 +0300

    Update StandardRoom.java

--- src/StandardRoom.java
+++ src/StandardRoom.java
@@ -1,4 +1,28 @@
+public class StandardRoom extends Room {
 
-public class StandardRoom {
+    private int numBeds;
+    private boolean hasTV;
 
+    public StandardRoom(int roomNumber, double price,
+                        int numBeds, boolean hasTV) {
+
+        super(roomNumber, price, "Standard");
+        this.numBeds = numBeds;
+        this.hasTV = hasTV;
+    }
+
+    public int getNumBeds() {
+        return numBeds;
+    }
+
+    public boolean hasTV() {
+        return hasTV;
+    }
+
+    public String toString() {
+
+        return super.toString()
+                + " | Beds: " + numBeds
+                + " | TV: " + hasTV;
+    }
 }
